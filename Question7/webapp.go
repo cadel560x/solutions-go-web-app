2d0e44dcc14fa511e2c15ea97b87ee831f5c5c8b
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-10-27 20:35:56 +0100

    Question7 use of 'template.HTML' to unescape HTML tags

--- Question7/webapp.go
+++ Question7/webapp.go
@@ -10,9 +10,12 @@ import (
 )
 
 type goPlaceholders struct {
-	Message   string
-	UserGuess string
-	NewGame   string
+	Message string
+	// As seen in https://stackoverflow.com/questions/18175630/go-template-executetemplate-include-html
+	UserGuess template.HTML
+	// Congratulations string
+	// NewGame template.HTML
+	// NewGameLink     string
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -25,26 +28,41 @@ func handlerGame(w http.ResponseWriter, r *http.Request) {
 	guess := r.FormValue("guess")
 	placeholders := &goPlaceholders{Message: "Guess a number between 1 and 20"}
 	rand.Seed(time.Now().UnixNano())
-	randomNumber := strconv.Itoa(rand.Intn(20-1) + 1)
+	outputHTML := ""
 
 	// Check if 'guess' variable and 'target' cookie are set
 	if len(guess) > 0 && len(targetCookie.Value) > 0 {
 		// User wins
 		if strings.Compare(targetCookie.Value, guess) == 0 {
-			placeholders.UserGuess = "Congratulations!! You've guessed the number " + guess + "!!!"
-			placeholders.NewGame = "Want to try again? New Game"
+			// placeholders.Congratulations = "Congratulations!! You've guessed the number!!" + guess
+			// placeholders.NewGame = "Want to try again? <a href=\"/guess\">New Game</a>"
+			// placeholders.NewGame = "Want to try again? <a href=\"/guess\">New Game</a>"
+			outputHTML += "<h3>Congratulations!! You've guessed the number!!</h3>\n"
+			outputHTML += "<h3>The number was " + guess + "</h3>\n"
+			outputHTML += "<h3>Want to try again? <a href=\"/guess\">New Game</a></3>\n"
+
+			// placeholders.UserGuess = template.HTML(outputHTML)
+
+			// Set new cookie for the next game
+			http.SetCookie(w, &http.Cookie{Name: "target", Value: strconv.Itoa(rand.Intn(20-1) + 1)})
+
 			// User loses
 		} else {
-			placeholders.UserGuess = "You guessed " + guess + ". "
+			outputHTML += "<h3>You guessed " + guess + "</h3>\n"
+
+			// Guess is high
 			if guess > targetCookie.Value {
-				placeholders.UserGuess += "Your guess is high."
+				outputHTML += "<h3>Your guess is high</h3>\n"
+				// Guess is low
 			} else {
-				placeholders.UserGuess += "Your guess is low."
+				outputHTML += "<h3>Your guess is low</h3>\n"
 			}
-		}
+		} // if - else
+
+		placeholders.UserGuess = template.HTML(outputHTML)
 		// Set cookie if not set
 	} else if targetCookie == nil {
-		http.SetCookie(w, &http.Cookie{Name: "target", Value: randomNumber})
+		http.SetCookie(w, &http.Cookie{Name: "target", Value: strconv.Itoa(rand.Intn(20-1) + 1)})
 	}
 
 	templ, _ := template.ParseFiles("guess.tmpl")
