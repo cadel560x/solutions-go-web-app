014033656ce8ca5942f8872940517b80efa5acfa
JAVIER MANTILLA - Student <G00329649@gmit.ie>
2017-10-27 16:46:03 +0100

    Question5 initial commit and guess.tmpl update

--- Question5/webapp.go
+++ Question5/webapp.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type messageT struct {
@@ -14,7 +17,15 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerGame(w http.ResponseWriter, r *http.Request) {
+	// Initializing local variables
 	message := &messageT{Message: "Guess a number between 1 and 20"}
+	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Intn(20-1) + 1
+
+	// Setting up cookie 'target'
+	if _, err := r.Cookie("target"); err != nil {
+		http.SetCookie(w, &http.Cookie{Name: "target", Value: strconv.Itoa(randomNumber)})
+	}
 
 	templ, _ := template.ParseFiles("guess.tmpl")
 	templ.Execute(w, message)
